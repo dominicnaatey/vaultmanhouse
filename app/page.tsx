72b990a2d00ee5d5e6f40de0af65b44e0bfbc015
dominicnaatey <dominicnaatey@gmail.com>
2026-06-25 21:09:39 +0000

    refactor(page): remove duplicate PortfolioSection and reorder sections

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
       <AboutSection />
       {/* <FeaturesStrip /> */}
       <ServicesSection />
+      <PortfolioSection />
       <AccordionSection />
       {/* <AgriPartnerSection /> */}
-      <PortfolioSection />
       <BlogSection />
     </>
   );
