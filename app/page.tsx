1ec5c74b3be8c305d34706d7f49dfb1402480ae8
dominicnaatey <dominicnaatey@gmail.com>
2026-06-25 21:13:50 +0000

    refactor(page): replace portfolio with commodities section

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import FeaturesStrip from '@/components/FeaturesStrip';
 import ServicesSection from '@/components/ServicesSection';
 import AccordionSection from '@/components/AccordionSection';
 import AgriPartnerSection from '@/components/AgriPartnerSection';
-import PortfolioSection from '@/components/PortfolioSection';
+import CommoditiesSection from '@/components/Commodities';
 import BlogSection from '@/components/BlogSection';
 
 export default function Home() {
@@ -14,7 +14,7 @@ export default function Home() {
       <AboutSection />
       {/* <FeaturesStrip /> */}
       <ServicesSection />
-      <PortfolioSection />
+      <CommoditiesSection />
       <AccordionSection />
       {/* <AgriPartnerSection /> */}
       <BlogSection />
